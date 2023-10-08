553d7e614b4eeaf4badfd7f8aab833a667335609
Alexia Sander <sander.alexia@gmail.com>
2023-10-08 09:24:23 +0900

    wines controller methods new create and show added

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,4 +1,20 @@
 class WinesController < ApplicationController
+  def new
+    @wine = Wine.new
+  end
+
+  def create
+    @wine = Wine.new(wine_params)
+    @wine.user = current_user
+
+    if @wine.save
+      flash[:notice] = 'Votre vin a été ajouté.'
+      redirect_to @wine
+    else
+      render :new
+    end
+  end
+
   def index
     @user = current_user
     @wines = @user.wines
@@ -11,4 +27,15 @@ class WinesController < ApplicationController
       @wines = current_user.wines.order(created_at: :desc)
     end
   end
+
+  def show
+    @wine = Wine.find(params[:id])
+  end
+
+  private
+
+  def wine_params
+    params.require(:wine).permit(:name, :wine_type, :country, :region, :winery, :variety, :year, :quantity, :rating, :empty, :comment)
+  end
+
 end
